6bcc200e6a26b88636c8bc621787deb0b5022608
Sati <s.urkumbayeva@mail.ru>
2023-07-01 14:29:59 +0600

    Buttons for changing the quantity of goods

--- js/cart.js
+++ js/cart.js
@@ -11,16 +11,54 @@ $.getJSON('goods.json', function(data) {
         var out = '';
         for (var key in cart) {
 
-            out += '<button class="delete">x</button>';
+            out+='<div class="cart-goods">';
+            out += '<button class="delete" data-art="'+key+'">x</button>';
             out += '<img src="'+goods[key].img+'" width="48">';
             out += goods[key].name;
-            out += '<button class="minus">-</button>';
+            out += '<button class="minus" data-art="'+key+'">-</button>';
             out += cart[key];
-            out += '<button class="plus">+</button>';
-            out += cart[key]*goods[key].cost;
-            out += '<br>';
+            out += '<button class="plus" data-art="'+key+'">+</button>';
+            out += '<p>'+cart[key]*goods[key].cost+' tg</p>';
+            // out += '<br>';
+            out+='</div>';
         }
         $('#my-cart').html(out);
+        $('.plus').on('click', plusGoods);
+        $('.minus').on('click', minusGoods);
+        $('.delete').on('click', deleteGoods);
+    }
+
+    function plusGoods() {
+
+        var articul = $(this).attr('data-art');
+        cart[articul]++;
+
+        saveCartToLS(); // сохраняю корзину в localStorage
+        showCart();
+    }
+
+    function minusGoods() {
+
+        var articul = $(this).attr('data-art');
+        if (cart[articul] > 1) {
+
+            cart[articul]--;
+        } else {
+
+            delete cart[articul];
+        }
+        
+        saveCartToLS(); // сохраняю корзину в localStorage
+        showCart();
+    }
+
+    function deleteGoods() {
+
+        var articul = $(this).attr('data-art');
+        delete cart[articul];
+        
+        saveCartToLS();
+        showCart();
     }
 });
 
@@ -30,4 +68,9 @@ function checkCart() {
 
         cart = JSON.parse(localStorage.getItem('cart'));
     }
+}
+
+function saveCartToLS() {
+
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
