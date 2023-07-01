970999ee9b62635f19c06207cd546f4183ed0edd
Sati <s.urkumbayeva@mail.ru>
2023-07-01 11:30:21 +0600

    Create a shopping cart

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+var cart = {}; // моя корзина
+
 $('document').ready(function() {
 
     loadGoods();
@@ -15,9 +17,24 @@ function loadGoods() {
             out+='<p>Price: '+data[key]['cost']+' tg</p>';
             out+='<img src="'+data[key].img+'">';
             // out+='<p>'+data[key]['description']+'</p>';
-            out+='<button>Buy</button>';
+            out+='<button class="add-to-cart" data-art="'+key+'">Buy</button>';
             out+='</div>';
         }
         $('#goods').html(out);
+        $('button.add-to-cart').on('click', addToCart);
     });
+}
+
+function addToCart() {
+    // добавляю товар в корзину
+    var articul = $(this).attr('data-art');
+    if (cart[articul] !=undefined) {
+
+        cart[articul]++;
+    } else {
+
+        cart[articul] = 1;
+    }
+    
+    console.log(cart);
 }
